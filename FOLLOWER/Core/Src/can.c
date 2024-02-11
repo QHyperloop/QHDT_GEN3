0623f004ea8eb58aa27bf2e25c6100212728aab7
Patrick <patrickb1210@gmail.com>
2024-02-11 15:28:35 -0500

    formatting stuff

--- FOLLOWER/Core/Src/can.c
+++ FOLLOWER/Core/Src/can.c
@@ -20,6 +20,8 @@ FDCAN_FilterTypeDef Pod;
 extern FDCAN_HandleTypeDef hfdcan1;
 extern FDCAN_HandleTypeDef hfdcan2;
 extern FDCAN_HandleTypeDef hfdcan3;
+uint8_t RxData_Pod[8];
+uint8_t ISO_STATE;
 
 
 uint8_t CAN_INIT(){
@@ -90,9 +92,42 @@ uint8_t Set_ESC_RPM(uint8_t esc_id, uint8_t RPM[4]){
 	return 0;
 }
 
+uint8_t IMD_Req_Isolation(){
+	TxHeader_Pod.Identifier = 0xA100101;
+	TxHeader_Pod.DataLength = FDCAN_DLC_BYTES_1;
+	uint8_t temp_data[] = {0xE0};
+	if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan3, &TxHeader_Pod, temp_data)!= HAL_OK){
+		return 1;
+	}
+	return 0;
+
+}
 
 
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs){
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK){
+		Error_Handler();
+	}
 
 
+}
+
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo1ITs){
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader_Pod, RxData_Pod) != HAL_OK){
+		Error_Handler();
+	}
+// HE NE HU - HV LV IS1 ISO
+	if(RxHeader_Pod.Identifier == 0xA100101){
+		if(RxData_Pod[1] & 0x03 == 0b10){
+			ISO_STATE = 0xF0; //Warning set LED to yellow
+			IMD_Req_Isolation();
+		}else if(RxData_Pod[1] & 0x03 == 0b01){
+			ISO_STATE = 0xFF; //fault set LED to Red and full estop
+		}else{
+			ISO_STATE = 0x00; //all good
+
+		}
+	}
+}
 
 
