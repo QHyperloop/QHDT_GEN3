534288ec5ed42b31ca7be174e0e0826707f10118
Patrick <patrickb1210@gmail.com>
2024-05-25 11:49:02 -0400

    Update main.c

--- MAIN/main.c
+++ MAIN/main.c
@@ -705,7 +705,9 @@ static int callback_websockets(struct lws *wsi, enum lws_callback_reasons reason
             }
 
             unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + strlen(response) + LWS_SEND_BUFFER_POST_PADDING];
-			unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING+1]=response;
+            buf[LWS_SEND_BUFFER_PRE_PADDING] = response;
+			unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+            
 			size_t n = sprintf( (char *)p, "%u", rand() );
 
             lws_write(wsi, p, n, LWS_WRITE_TEXT);
