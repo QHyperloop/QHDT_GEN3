3d45bd7d883d23fbe03e5cdea55e51328fc54961
Patrick <patrickb1210@gmail.com>
2024-06-03 17:26:07 -0400

    Reapply "Update index.js"
    This reverts commit 90cf8e4ff266268940554d47a108ab9e25fca1f3.

--- MAIN/index.js
+++ MAIN/index.js
@@ -55,12 +55,10 @@ const { WebSocketServer } = require('ws');
 const http = require('http');
 const path = require('path');
 
-
 const app = express();
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home-screen.html'));
@@ -82,7 +80,7 @@ app.get('/settings', (req, res) => {
 const server = http.createServer(app);
 
 // Attach WebSocket server to the HTTP server
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
     console.log('New client connected');
