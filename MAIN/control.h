26054446444f2065c66ba1b41b11dd21431bd0b8
Patrick <patrickb1210@gmail.com>
2024-06-16 22:04:50 -0400

    Update control.h

--- MAIN/control.h
+++ MAIN/control.h
@@ -17,14 +17,14 @@
 #include <libwebsockets.h>
 #include <json-c/json.h>
 
-double sensors_data[6] = {0};
-int Fault_Flag = 0;
-uint8_t ISO_STATE;
+extern double sensors_data[6] = {0};
+extern int Fault_Flag = 0;
+extern uint8_t ISO_STATE;
 
-int sensor_flag = 0;
-int response_flag = 0;
+extern int sensor_flag = 0;
+extern int response_flag = 0;
 
-int success_or_fail = 0;
+extern int success_or_fail = 0;
 
 typedef enum _error_handler {
 	CAN_INIT_OK,
@@ -64,5 +64,7 @@ typedef enum _PodState{
 	CRAWL,
 	TRACK
 }PodState;
-extern PodState Curr_State = INIT;
+extern PodState Curr_State;
 extern uint8_t ISO_STATE;
+
+Curr_State = INIT;
